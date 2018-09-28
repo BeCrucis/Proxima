6563a9abab256514bea09654933c6efcff399aeb
BeCrucis <edwardpara910@gmail.com>
2018-09-27 22:02:50 -0500

    Gauss : Comentarios extra

--- C++/ATerm/Gauss.cpp
+++ C++/ATerm/Gauss.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-void mostrar(long double x[][10], int cx, int lx, string prompt = "Resultado: "){
+void mostrar(long double x[][10], int cx, int lx, string prompt = "Resultado: "){ //Esta es una funcion que muestra matrices, por ahora puede ser ignorada
 
     cout << endl << endl << prompt << endl;
     for (int i = 1; i <= lx; ++i) {
@@ -36,27 +36,34 @@ int main(){
 	cout << "Inserte la cantidad de columnas de su matriz x: ";
 	cin >> cx;
 
-    cx = cx + 1;
+    cx = cx + 1; // Esto añade una columna, que sera la que almacena las constantes
 
-	for (int i = 1; i <= lx; i++) {
+	for (int i = 1; i <= lx; i++) { //Pide que se inserte la matriz
         cc = 0;
 		for (int j = 1; j <= cx; j++) {
-     
-            cout << "Inserte la coordenada [" << i << "][" << j << "] : ";
-			cin >> x[i][j];
             
+            if(j != cx){
+                cout << "Inserte la variable en [" << i << "][" << j << "] : ";
+                cin >> x[i][j];
+            }
+            else{
+                cout << "Inserte la solucion en la coordenada [" << i << "][" << j << "] : ";
+                cin >> x[i][j];
+            }
+
 		}
 	}
 
-	mostrar(x, cx , lx , "Su matriz es: ");
+	mostrar(x, cx , lx , "Su matriz es: "); // Muestra el sistema de ecuaciones insertado
 
-    if(lx > cx){
+    if(lx > cx){ //Verifica si hay mas variables que constantes, si es asi, la matriz tiene infinitas soluciones
         cout << "La matriz tiene soluciones infinitas" << endl;
         system("pause");
         return 0;
     }
 //WIP
     cc = 1; //Columna clave, columna en que debe aparecer un 1 en la matriz en funcion de la fila analizada
+
     for(int i = 1; i <= lx;++i ){
         
         if(cc == i && x[i][cc] == 0){ //Trata de arreglar las cosas si la variable que debe ser 1 es 0
@@ -82,11 +89,11 @@ int main(){
                 cout << "Problema encontrado en la fila " << i << endl;
                 cout << "La matriz o tiene soluciones infinitas, o no tiene, verificando . . ." << endl;
 
-                if(x[lx][cx] != 0){
+                if(x[lx][cx] != 0){ //Busca si existe una inconsistencia, si es asi, no existen soluciones
                     cout << "El sistema no tiene soluciones! " << endl;
                 }
 
-                else{
+                else{ //De lo contrario, hay infinitas soluciones
                     cout << "El sistema tiene infinitas soluciones!" << endl;
                 }
                 system("pause");
@@ -106,26 +113,28 @@ int main(){
    
         }
 
-        for (int n = 1;n <= lx; ++n ){
+        for (int n = 1;n <= lx; ++n ){ // Ahora reduzco el resto de elementos de la columna a 0 para obtener la forma escalonada completa
 
             if(n != i){
                 nm = x[i][cc] * x[n][cc] * -1; //numero hecho para reducir el resto de elementos de la columna a 0
 
                 for(int c = 1; c <= cx; ++c){
-                    x[n][c] = (x[i][c] * nm) + x[n][c];
+                    x[n][c] = (x[i][c] * nm) + x[n][c]; //nm veces la fila fi + la fila fn
                 }             
             }
         }
-    cc += 1;
+    cc += 1; //Pasa a la siguiente columna clave, en resumen, hace que los 1 esten en diagonal, 
+             //creando una matriz identidad con las constantes a la derecha
 
-    string paso = string("Paso numero ") + to_string(i);
+    string paso = string("Paso numero ") + to_string(i); //Muestra la matriz con la fila i despejada
 
     mostrar(x, cx, lx , paso);
-
+    cout << endl;
     }
 
 
-    mostrar(x, cx, lx);
+    mostrar(x, cx, lx); //Muestra la matriz resultado
 
     system("pause");
+    return 0;
 }
