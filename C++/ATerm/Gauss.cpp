2a207897a3e7ec309e068a0e707505b1ee4bff37
BeCrucis <edwardpara910@gmail.com>
2018-09-27 20:52:23 -0500

    Mejoras a Gauss 3.0

--- C++/ATerm/Gauss.cpp
+++ C++/ATerm/Gauss.cpp
@@ -2,13 +2,34 @@
 #include <math.h>
 #include <windows.h>
 #include <iomanip>
+#include <string>
 
 using namespace std;
 
+void mostrar(double x[][10], int cx, int lx, string prompt = "Resultado: "){
+
+    cout << endl << endl << prompt << endl;
+    for (int i = 1; i <= lx; ++i) {
+		for (int j = 1; j <= cx; ++j) {
+			cout << right << setw(2) << "| "
+				<< right << setw(9) << x[i][j] << " |";
+		}
+
+		cout <<  endl;
+    }
+
+    for (int n = 1; n <= cx; ++n) {
+
+			cout << "---------";
+		}
+		cout << endl;
+    
+}
 int main(){
 
-    double x[10][10], di, nm;
+    double x[10][10], di, nm, t[10];
     int lx, cx, cc;
+    bool found;
 
     cout << "Inserte la cantidad de filas de su matriz x: ";
 	cin >> lx;
@@ -27,20 +48,43 @@ int main(){
 		}
 	}
 
-	cout << "Su matriz es: " << endl;
-	for (int i = 1; i <= lx; ++i) {
-		for (int j = 1; j <= cx; ++j) {
-			cout << right << setw(2) << "| "
-				<< right << setw(5) << x[i][j] << " |";
-		}
+	mostrar(x, cx , lx , "Su matriz es: ");
 
-		cout << endl;
+    if(lx > cx){
+        cout << "La matriz tiene soluciones infinitas" << endl;
+        system("pause");
+        return 0;
     }
 //WIP
     cc = 1; //Columna clave, columna en que debe aparecer un 1 en la matriz en funcion de la fila analizada
     for(int i = 1; i <= lx;++i ){
+        
+        if(cc == i && x[i][cc] == 0 && i < lx){ //Trata de arreglar las cosas si la variable que debe ser 1 es 0
 
-        if(cc == i || x[i][cc] != 1){ // Verifico si la coordenada es 1, si no lo es, multiplico toda la fila por su inversa
+            found = false;
+
+            for( int j = i + 1; j <= lx; ++j){
+
+                if(x[j][cc] > 0){
+                    for(int k = 1; k <= cx; ++k){
+
+                        t[k] = x[i][k];
+                        x[i][k] = x[j][k];
+                        x[j][k] = t[k];
+                    }
+                found = true;
+                }
+            }
+
+            if(found == false){
+                cout << "Problema encontrado en la fila " << i << endl;
+                cout << "La matriz o tiene soluciones infinitas, o no tiene, verificando . . ." << endl;
+                system("pause");
+                return 0;
+            }
+        }
+
+        if(cc == i && x[i][cc] != 1){ // Verifico si la coordenada es 1, si no lo es, multiplico toda la fila por su inversa
 
             di = 1/x[i][cc]; //Inversa del punto de la matriz que debe convertirse en 1
 
@@ -55,7 +99,7 @@ int main(){
         for (int n = 1;n <= lx; ++n ){
 
             if(n != i){
-                nm = x[i][cc] * x[n][cc] * -1;
+                nm = x[i][cc] * x[n][cc] * -1; //numero hecho para reducir el resto de elementos de la columna a 0
 
                 for(int c = 1; c <= cx; ++c){
                     x[n][c] = (x[i][c] * nm) + x[n][c];
@@ -63,10 +107,8 @@ int main(){
             }
         }
     cc += 1;
-    }
 
-
-    cout << endl << endl << "Resultado: " << endl;
+    cout << endl << endl << "Paso numero "<< i << " : " << endl;
     for (int i = 1; i <= lx; ++i) {
 		for (int j = 1; j <= cx; ++j) {
 			cout << right << setw(2) << "| "
@@ -75,6 +117,11 @@ int main(){
 
 		cout << endl;
     }
+
+    }
+
+
+    mostrar(x, cx, lx);
 
     system("pause");
 }
