8911088d6557f861e26ab503b1ec6b68ce79199d
Gacrucis <Edwardpara910@gmail.com>
2019-11-21 22:59:35 -0500

    Finished Sorter

---
+++ PY/ETerm/Sorter/Persona.py
@@ -0,0 +1,86 @@
+from collections import Counter
+
+class Persona():
+
+    def __init__(self, doc, sur1, sur2, name1, name2, genre, bday, code):
+
+        if doc is None:
+            self.document = ""
+        else:
+            self.document = doc
+        
+        if sur1 is None:
+            self.sur1 = ""
+        else:
+            self.sur1 = sur1.upper()
+        
+        if sur2 is None:
+            self.sur2 = ""
+        else:
+            self.sur2 = sur2.upper()
+
+        if name1 is None:
+            self.name1 = ""
+        else:
+            self.name1 = name1.upper()
+
+        if name2 is None:
+            self.name2 = ""
+        else:
+            self.name2 = name2.upper()
+
+        if genre is None:
+            self.genre = ""
+        else:
+            self.genre = genre
+
+        if bday is None:
+            self.bday = ""
+        else:
+            self.bday = bday
+
+        if code is None:
+            self.code = ""
+        else:
+            self.code = code
+        
+        self.events = []
+
+    def add_event(self, event, is_signed, time1, time2, time3, lector, auditorium):
+
+        if event is None:
+            event = ""
+        
+        if is_signed is None:
+            is_signed = ""
+        
+        if lector is None:
+            lector = ""
+        
+        if auditorium is None:
+            auditorium = ""
+
+        event_info = [event, is_signed, time1, time2, time3, lector, auditorium]
+
+        self.events.append(event_info)
+
+        self.events.sort(key=lambda x: x[2])
+    
+    def get_events_validity(self):
+
+        if len(self.events)%2 != 0:
+            return False
+
+        prev = "Salida"
+
+        for event in self.events:
+
+            t = event[0]
+            if t == prev:
+                return False
+            prev = t
+        
+        return True
+
+       
+        
---
+++ PY/ETerm/Sorter/sorter.py
@@ -0,0 +1,99 @@
+import os
+import openpyxl
+from Persona import *
+from openpyxl.styles import Color, PatternFill, Font, Border
+from openpyxl.styles import colors
+
+EXCEL_PAGE = "input.xlsx"
+
+def main():
+    print_title()
+
+    wb = openpyxl.load_workbook(EXCEL_PAGE)
+    ws = wb.active
+
+    people = []
+    documents = []
+
+    h = 0
+    n = 0
+
+    for row in ws.iter_rows(2, ws.max_row, 1, 15):
+        doc = row[0].value
+
+        if doc is None:
+            n += 1
+            continue
+
+        if doc not in documents:
+            people.append(Persona(doc, row[1].value, row[2].value, row[3].value, row[4].value, row[5].value, row[6].value, row[8].value))
+            documents.append(doc)
+        
+        person_index = search_person(people, doc)
+        people[person_index].add_event(row[7].value, row[9].value, row[10].value, row[11].value, row[12].value, row[13].value, row[14].value)
+        h += 1
+
+    output_wb = openpyxl.Workbook()
+    output_ws = output_wb.active
+
+    row_number = 2
+
+    for person in people:
+        fill = False
+        
+        if not person.get_events_validity():
+            fill = True
+        
+        for event in person.events:
+            output_ws.cell(row_number, 1).value = person.document
+            output_ws.cell(row_number, 2).value = person.sur1
+            output_ws.cell(row_number, 3).value = person.sur2
+            output_ws.cell(row_number, 4).value = person.name1
+            output_ws.cell(row_number, 5).value = person.name2
+            output_ws.cell(row_number, 6).value = person.genre
+            output_ws.cell(row_number, 7).value = person.bday
+            output_ws.cell(row_number, 8).value = event[0]
+            output_ws.cell(row_number, 9).value = person.code
+            output_ws.cell(row_number, 10).value = event[1]
+            output_ws.cell(row_number, 11).value = event[2]
+            output_ws.cell(row_number, 12).value = event[3]
+            output_ws.cell(row_number, 13).value = event[4]
+            output_ws.cell(row_number, 14).value = event[5]
+            output_ws.cell(row_number, 15).value = event[6]
+
+            if fill:
+                print("O NO")
+                redFill = PatternFill(start_color='FFFF0000',
+                end_color='FFFF0000',
+                fill_type='solid')
+                for row in output_ws.iter_rows(row_number, row_number, 1, 15):
+                    for cell in row:
+                        cell.fill = redFill
+
+            row_number += 1
+
+    output_wb.save("Output.xlsx")
+
+
+
+        
+def print_title():
+    print(r"""  ____     U  ___ u   ____      _____  U _____ u   ____     
+ / __"| u   \/"_ \/U |  _"\ u  |_ " _| \| ___"|/U |  _"\ u  
+<\___ \/    | | | | \| |_) |/    | |    |  _|"   \| |_) |/  
+ u___) |.-,_| |_| |  |  _ <     /| |\   | |___    |  _ <    
+ |____/>>\_)-\___/   |_| \_\   u |_|U   |_____|   |_| \_\   
+  )(  (__)    \\     //   \\_  _// \\_  <<   >>   //   \\_  
+ (__)        (__)   (__)  (__)(__) (__)(__) (__) (__)  (__) """)
+
+def search_person(people, document):
+
+    i = 0
+    for person in people:
+        if person.document == document:
+            return i
+        
+        i += 1
+
+if __name__ == "__main__":
+    main()
