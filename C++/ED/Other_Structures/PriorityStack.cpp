6092ca6b41acd364b544706876977b454f04b817
Ed <edwardpara910@gmail.com>
2020-02-26 09:48:20 -0500

    Added PriorityStack

---
+++ C++/ED/Other_Structures/PriorityStack.cpp
@@ -0,0 +1,236 @@
+#include <iostream>
+#include <iomanip>
+#include <windows.h>
+
+using namespace std;
+
+struct Nodo {
+    int dato;
+    int prioridad;
+    Nodo *sig;
+};
+
+class Pila {
+
+    private:
+
+    Nodo *cima;
+
+    public:
+
+    Pila(){
+        this->cima = NULL;
+    }
+
+    ~Pila(){
+
+        Nodo *objetivo = cima;
+        Nodo *temp;
+        int datoEliminado;
+
+        while (objetivo != NULL){
+            temp = objetivo;
+            datoEliminado = objetivo->dato;
+
+            objetivo = objetivo->sig;
+            delete temp;
+            cout << "Dato: ["<<datoEliminado<< "] Eliminado!" << endl;
+            
+        }
+    }
+
+    void meter(){
+        
+        Nodo *nuevo = new Nodo();
+
+        cout << "Inserte el dato agregar: ";
+        cin >> nuevo->dato;
+
+        cout << "Inserte el la prioridad del dato: ";
+        cin >> nuevo->prioridad;
+
+        if(cima == NULL){
+            // Agrega el primer nodo
+            cima = nuevo;
+            cima->sig = NULL;
+        } else {
+
+            Nodo *anterior = buscarPrioridad(nuevo->prioridad);
+            
+            if(anterior == NULL){
+                // Agrega el ultimo nodo
+                nuevo->sig = cima;
+                cima = nuevo;
+            } else {
+                // Agrega un nodo intermedio
+                nuevo->sig = anterior->sig;
+                anterior->sig = nuevo;
+            }
+        }
+
+    }
+
+    Nodo *buscarPrioridad(int prioridad){
+
+        // Obtiene el nodo anterior al nodo de insercion 
+
+        Nodo *anterior;
+		anterior = NULL;
+
+		if (cima->prioridad <= prioridad){
+
+            return anterior;
+
+        } else {
+
+			anterior = cima;
+
+			while ((anterior->sig != NULL) && (anterior->sig->prioridad > prioridad)){
+
+				anterior = anterior->sig;
+
+			}
+													
+			return anterior;
+		}
+    }
+
+    void sacar(){
+
+        Nodo *objetivo = cima;
+
+        if (objetivo == NULL){
+            cout << "La pila esta vacia!" << endl;
+            return;
+        }
+
+        cima = objetivo->sig;
+
+        cout << "Dato sacado: ["<<objetivo->dato<<"]"<< endl;
+    }
+
+    void limpiar(){
+
+        Nodo *objetivo = cima;
+        Nodo *temp;
+        int datoEliminado;
+
+        while (objetivo != NULL){
+            temp = objetivo;
+            datoEliminado = objetivo->dato;
+
+            objetivo = objetivo->sig;
+            delete temp;
+            cout << "Dato: ["<<datoEliminado<< "] Eliminado!" << endl;
+        }
+
+        cima = NULL;
+    }
+
+    void mostrarCima(){
+
+        if(cima == NULL){
+            cout << "La pila esta vacia!" << endl;
+            return;
+        }
+
+        cout << "La cima es: ["<<cima->dato<<"]"<< endl;
+    }
+
+    void longitud(){
+
+        int longitud = 0;
+
+        Nodo *objetivo = cima;
+
+        while(objetivo != NULL){
+            longitud += 1;
+            objetivo = objetivo->sig;
+        }
+
+        cout << "El largo de la pila es: ["<<longitud<<"]"<< endl;
+        
+    }
+
+    void desplegar(){
+
+        if(cima == NULL){
+			cout << "[]" << endl;
+			return;
+		}
+
+		string out = "[";
+        Nodo *nodoActual;
+        nodoActual = cima;
+
+        while (nodoActual->sig != NULL){
+
+            out = out + std::to_string(nodoActual->dato) + ", ";
+            nodoActual = nodoActual->sig;
+
+        }
+
+        out = out + std::to_string(nodoActual->dato) + "]";
+
+        cout << out << endl;
+    }
+
+};
+
+int main(){
+
+    Pila pila;
+    int opcion;
+
+    do {
+
+        cout << "Pila con prioridad!" << endl << endl;
+        cout << "1. Agregar objeto a la pila" << endl;
+        cout << "2. Sacar objeto con prioridad" << endl;
+        cout << "3. Limpiar la pila" << endl;
+        cout << "4. Mostrar cima de la pila" << endl;
+        cout << "5. Mostrar largo de la pila" << endl;
+        cout << "6. Mostrar la pila" << endl;
+        cout << "99. Salir" << endl;
+        cout << endl;
+
+        cout << "Inserte su opcion: ";
+        cin >> opcion;
+        cout << endl;
+
+        if (opcion == 1){
+            
+            pila.meter();
+
+        } else if (opcion == 2){
+
+            pila.sacar();
+
+        } else if (opcion == 3){
+
+            pila.limpiar();
+
+        } else if (opcion == 4){
+
+            pila.mostrarCima();
+
+        } else if (opcion == 5){
+
+            pila.longitud();
+
+        } else if (opcion == 6){
+        
+
+        
+        }
+
+        cout << "Pila actual: ";
+        pila.desplegar();  
+        cout << endl;
+        
+        system("pause");
+        system("cls");
+       
+    } while (opcion != 99);
+
+}
